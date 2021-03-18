53315cccb2491cb64e8688c6c762b6467260c509
tyomaz <52513279+tyomaz@users.noreply.github.com>
2021-03-18 22:15:53 +0300

    Update install1.sh

--- install1.sh
+++ install1.sh
@@ -1,22 +1,14 @@
 #!/bin/bash
 
-SERVICE_NAME='timeout-log'
-SCRIPT_DIR_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-SCRIPT_PATH="$SCRIPT_DIR_PATH/timeout-logger.sh"
-chmod +x "$SCRIPT_DIR_PATH"
+LOG_PATH='/home/slava/Source/ITS/tm.log'
+echo /dev/null > "$LOG_PATH"
 
-export DB_PATH="$SCRIPT_DIR_PATH"
-python "$SCRIPT_DIR_PATH/migrate_base.py"
+echo "Scrip started. For kill script push Ctrl+C"
 
-cat > "/etc/systemd/system/$SERVICE_NAME.service" <<EOF
-[Unit]
-Description=Timeout logger
-
-[Service]
-ExecStart=$SCRIPT_PATH
-
-[Install]
-WantedBy=multi-user.target
-EOF
-
-systemctl start "$SERVICE_NAME"
+while (true)
+do
+    current_date_time="`date "+%Y-%m-%d %H:%M:%S"`"
+    echo "[$current_date_time]: im allive" >> "$LOG_PATH"
+    sleep 1;
+    echo "1 second"
+done;
