2364954226da5be83c6d42ca274180a8d4472acb
tyomaz <52513279+tyomaz@users.noreply.github.com>
2021-03-18 22:27:21 +0300

    Update install1.sh

--- install1.sh
+++ install1.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-LOG_PATH='/home/slava/Source/ITS/tm.log'
+LOG_PATH='/home/Source/ITS/test.log'
 echo /dev/null > "$LOG_PATH"
 
-echo "Scrip started. For kill script push Ctrl+C"
+echo "started, for stop push Ctrl+C"
 
-while (true)
-do
+    while (true)
+    do
     current_date_time="`date "+%Y-%m-%d %H:%M:%S"`"
-    echo "[$current_date_time]: im allive" >> "$LOG_PATH"
-    sleep 1;
-    echo "1 second"
-done;
+    echo "[$current_date_time]: privet vsem" >> "$LOG_PATH"
+    sleep 8;
+    echo "8 second"
+    done;
